3cdfff7815f4b4ff0187634f99d7079ca5c645ec
Andrew Pouliot <andrew@darknoon.com>
2011-07-26 00:39:29 -0700

    Fix no default clear of rendering destination #2

--- Engine/Core/WMEngine.mm
+++ Engine/Core/WMEngine.mm
@@ -357,6 +357,10 @@ NSString *const WMEngineInterfaceOrientationArgument = @"interfaceOrientation";
 	
 	//TODO: abstract this state out
 	glViewport(0, 0, renderContext.boundFramebuffer.framebufferWidth, renderContext.boundFramebuffer.framebufferHeight);
+	
+	//Clear out the rendering context
+	glClearColor(0, 0, 0, 1);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	renderContext.modelViewMatrix = [WMEngine cameraMatrixWithRect:inBounds];
 	
