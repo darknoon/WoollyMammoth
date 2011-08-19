e5531b8c3972ca932c1a3ca7152dc7c834362c28
Andrew Pouliot <andrew@darknoon.com>
2011-08-18 17:43:14 -0700

    Made some tweaks to the quad particle system. Rotation quaternion is now normalized, so particles should no longer explode and disappear. Added input values for the radius and the particle size.

--- More Patches/Quad Particles/WMQuadParticleSystem.h
+++ More Patches/Quad Particles/WMQuadParticleSystem.h
@@ -29,46 +29,12 @@ struct WMQuadParticleVertex;
 	
 	WMVector3Port *inputRotation;
 	WMVector3Port *inputGravity;
+	WMNumberPort *inputRadius;
+	WMNumberPort *inputParticleSize;
 	
 	WMImagePort *inputTexture;
 	
 	WMRenderObjectPort *outputObject;
-	
-	//Internal
-@public
-
-	NSUInteger maxParticles;
-	//Evaluates the noise function for every nth particle
-	NSUInteger particleUpdateSkip;
-	//The current index modulo particleUpdateSkip to start
-	NSUInteger particleUpdateIndex;
-	WMQuadParticle *particles;
-	
-	WMQuadParticleVertex *particleVertices;
-	
-	//0 when no data, 1 when 1st buffer, filled 2 when both
-	int particleDataAvailable;
-	NSUInteger currentParticleVBOIndex;
-	WMStructuredBuffer *particleVertexBuffers[2];
-	
-	//Holds indices. always the same, as each particle is a quad! ie boring (0,1,2, 1,2,3 … )
-	WMStructuredBuffer *particleIndexBuffer;
-	
-	GLKVector4 startColor;
-	float deltaAlpha;
-	
-	GLKVector3 particleCentroid;
-	
-	float turbulence;
-	
-	double t;
-	
-	//Potentially slower. Try turning off for performance
-	BOOL zSortParticles;
-	
-	WMShader *shader;
-	WMTexture2D *defaultTexture;
-	WMRenderObject *renderObject;
 }
 
 @end
