8b44c2e658db63829b4dbb65520ff9e1cf16b74d
Andrew Pouliot <andrew@darknoon.com>
2011-11-08 15:13:58 -0800

    Added some warnings where a uniform is left un-set.

--- Engine/Render/WMEAGLContext.m
+++ Engine/Render/WMEAGLContext.m
@@ -288,6 +288,10 @@
 			[textures addObject:value];
 			int textureUnit = [textures indexOfObject:value];
 			[shader setIntValue:textureUnit forUniform:uniformName];
+		} else if (value) {
+			DLog(@"Unsupported value %@ of class %@ for uniform name %@", value, [value class], uniformName);
+		} else {
+			DLog(@"WARNING: trying to render without a value for uniform %@. This can lead to unexpecetd results.", value, [value class], uniformName);
 		}
 	}
 	
