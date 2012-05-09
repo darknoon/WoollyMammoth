9f4c6fb9082d92b899bbd19234f093b98c24b384
Andrew Pouliot <andrew@darknoon.com>
2012-05-09 15:27:54 -0700

    Allow CGRoundRect to compile on Mac.

--- WMEdit/WMEdit/Util/CGRoundRect.h
+++ WMEdit/WMEdit/Util/CGRoundRect.h
@@ -1,5 +1,9 @@
 
+#if TARGET_OS_IPHONE
 #import <CoreGraphics/CoreGraphics.h>
+#elif TARGET_OS_MAC
+#import <ApplicationServices/ApplicationServices.h>
+#endif
 
 extern void CGContextAddRoundRect(CGContextRef context, CGRect rect, float radius);
 
