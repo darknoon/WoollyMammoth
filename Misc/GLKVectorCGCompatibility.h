7689fe15403acebf187be165e5935fafe40ca176
Andrew Pouliot <andrew@darknoon.com>
2012-07-27 22:12:46 -0700

    Add some more utils.

--- Misc/GLKVectorCGCompatibility.h
+++ Misc/GLKVectorCGCompatibility.h
@@ -40,5 +40,34 @@ static __inline__ GLKVector4 GLKVector4FromCGPoint(CGPoint v) {
 	return (GLKVector4){v.x, v.y, 0.0f, 0.0f};
 }
 
+//GLKVec* to CGSize
+
+static __inline__ CGSize CGSizeFromGLKVector2(GLKVector2 v) {
+	return (CGSize){v.x, v.y};
+}
+
+static __inline__ CGSize CGSizeFromGLKVector3(GLKVector2 v) {
+	return (CGSize){v.x, v.y};
+}
+
+static __inline__ CGSize CGSizeFromGLKVector4(GLKVector2 v) {
+	return (CGSize){v.x, v.y};
+}
+
+//CGSize to GLKVec*
+
+static __inline__ GLKVector2 GLKVector2FromCGSize(CGSize v) {
+	return (GLKVector2){v.width, v.height};
+}
+
+static __inline__ GLKVector3 GLKVector3FromCGSize(CGSize v) {
+	return (GLKVector3){v.width, v.height, 0.0f};
+}
+
+static __inline__ GLKVector4 GLKVector4FromCGSize(CGSize v) {
+	return (GLKVector4){v.width, v.height, 0.0f, 0.0f};
+}
+
+
 #endif
 
