c49f5709e0ec8ecd0f6912c8d0afc6abbb7028c4
Andrew Pouliot <andrew@darknoon.com>
2012-12-27 00:04:35 -0800

    Iconset update

---
+++ Application Mac/WMView.h
@@ -0,0 +1,13 @@
+//
+//  WMView.h
+//  WMGraph
+//
+//  Created by Andrew Pouliot on 12/26/12.
+//  Copyright (c) 2012 Darknoon. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WMView : NSView
+
+@end
---
+++ Application Mac/WMView.m
@@ -0,0 +1,28 @@
+//
+//  WMView.m
+//  WMGraph
+//
+//  Created by Andrew Pouliot on 12/26/12.
+//  Copyright (c) 2012 Darknoon. All rights reserved.
+//
+
+#import "WMView.h"
+
+@implementation WMView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
